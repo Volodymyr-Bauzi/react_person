37100c0b6421632fba6a168fa17f16d39b6f2857
Bauzi <volodiasyd@gmail.com>
2025-09-30 21:19:17 +0300

    Solution #4

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,8 +1,7 @@
 // export const Person = ({ person }) => ();
 export const Person = ({ person }) => {
-  const { name, age, partnerName, sex } = person;
+  const { name, age, partnerName, sex, isMarried } = person;
   const hasAge = age !== undefined;
-  const isMarried = partnerName !== undefined;
   const partnerLabel = sex === 'm' ? 'wife' : 'husband';
   const partnerMessage = isMarried
     ? `${partnerName} is my ${partnerLabel}`
