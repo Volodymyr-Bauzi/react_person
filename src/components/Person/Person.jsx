399d10e380167aeba77e1b358c3eef0105a26335
Bauzi <volodiasyd@gmail.com>
2025-09-30 22:15:02 +0300

    Solution #8

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,25 +1,32 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import './Person.scss';
+
 export const Person = ({ person }) => {
-  const { name, age, partnerName, sex } = person;
-  const hasAge = age !== undefined && age !== null;
-  const isMarried = Boolean(partnerName);
-  let partnerLabel;
+  const { name, age, sex, isMarried, partnerName } = person;
 
-  if (sex === 'm') {
-    partnerLabel = 'wife';
+  let partnerText = '';
+
+  if (isMarried) {
+    let partnerRole = '';
+
+    if (sex === 'm') {
+      partnerRole = 'wife';
+    } else {
+      partnerRole = 'husband';
+    }
+
+    partnerText = `${partnerName} is my ${partnerRole}`;
   } else {
-    partnerLabel = 'husband';
+    partnerText = 'I am not married';
   }
-
-  const partnerMessage = isMarried
-    ? `${partnerName} is my ${partnerLabel}`
-    : 'I am not married';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {hasAge && <p className="Person__age">{`I am ${age}`}</p>}
-      <p className="Person__partner">{partnerMessage}</p>
+      <h2 className="Person__name">My name is {name}</h2>
+
+      {age !== undefined && <p className="Person__age">I am {age}</p>}
+
+      <p className="Person__partner">{partnerText}</p>
     </section>
   );
 };
